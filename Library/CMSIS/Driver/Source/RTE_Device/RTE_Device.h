ede5832a16503ff8acfd6cfa57204bdb7e9c092d
Wayne Lin <wosayttn@gmail.com>
2025-08-30 20:24:13 +0800

    Commit at 2025-08-30T12:24:13Z

---
+++ Library/CMSIS/Driver/Source/RTE_Device/RTE_Device.h
@@ -0,0 +1,19 @@
+
+/**************************************************************************//**
+ * @file     RTE_Device.h
+ * @version  V1.00
+ * @brief    RTE Device Configuration for Nuvoton M55M1
+ *
+ * @copyright SPDX-License-Identifier: Apache-2.0
+ * @copyright Copyright (C) 2024 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+
+//-------- <<< Use Configuration Wizard in Context Menu >>> --------------------
+
+#ifndef __RTE_DEVICE_H
+#define __RTE_DEVICE_H
+
+#include "RTE_Device_I2C.h"
+
+
+#endif  /* __RTE_DEVICE_H */
