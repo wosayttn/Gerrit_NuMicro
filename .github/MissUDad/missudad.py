bec948c11b8c10308e21b3621a16e26c37ed4746
Wayne Lin <wosayttn@gmail.com>
2025-10-03 20:18:21 +0800

    Commit at 2025-10-03T12:18:21Z

---
+++ .github/MissUDad/missudad.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import shutil
+
+# Define prefix string of IP name you preferred.
+# EX: IP_LIST=[ 'UART', 'SPI', 'CLK', 'SYS' ]
+# Or append '*' symbol to build all projects.
+IP_LIST=[ '*' ]
+
+# Define VCOM port number of NuMaker board.
+COMX='COM6'
+
+# Define running interval between SampleCodes. (Default: 20 seconds)
+# Notice: The value depends on firmware flash time and SampleCode running time.
+RUNTIME=20
+
+# Define searching root directory
+PROJ_FOLDER_NAME='../../../SampleCode'
+
+'''
+Define your NuEclipse/GDB/OpenOCD installation path.
+'''
+OPENOCD_EXE='C:\\Program Files (x86)\\Nuvoton Tools\\OpenOCD\\bin\\openocd_cmsis-dap.exe'
+GDB_EXE='C:\\Program Files (x86)\\GNU Arm Embedded Toolchain\\10 2021.10\\bin\\arm-none-eabi-gdb.exe'
+ECLIPSE_EXE="C:\\Program Files (x86)\\Nuvoton Tools\\NuEclipse\\V1.02.025c\\NuEclipse\\eclipse\\eclipsec.exe"
+
+'''
+Define your IAR embedded workbench installation path.
+'''
+CSPYBAT_EXE='C:\\Program Files\\IAR Systems\\Embedded Workbench 9.4\\common\\bin\\cspybat'
+IARIDEPM_EXE='C:\\Program Files\\IAR Systems\\Embedded Workbench 9.4\\common\\bin\\IarIdePm.exe'
+IARBUILD_EXE="C:\\Program Files\\IAR Systems\\Embedded Workbench 9.4\\common\\bin\\iarbuild.exe"
+
+'''
+Define your Keil v537 installation path.
+'''
+UV4_EXE="C:\\Keil_v5\\UV4\\Uv4.exe"
+
+
+'''
+Don't touch me. Just collect module function here.
+'''
+def read_serial_port(serial_port):
+    while True:
+        if serial_port.in_waiting > 0:
+            data = serial_port.readline().decode('utf-8').strip()
+            print(data)
+
+def write_to_serial_port(serial_port):
+    while True:
+        message = input('$ ')
+        serial_port.write(message.encode('utf-8'))
+        print(message)
+
+def mirror_file(src_file, dst_file):
+    print('Copy ' + src_file+ ' to ' + dst_file)
+    dst_folder = os.path.dirname(dst_file)
+    if not os.path.exists(dst_folder):
+       os.makedirs(dst_folder)
+
+    shutil.copyfile(src_file, dst_file)
+
+def move_file(src_file, dst_file):
+    print('Move ' + src_file+ ' to ' + dst_file)
+    dst_folder = os.path.dirname(dst_file)
+    if not os.path.exists(dst_folder):
+       os.makedirs(dst_folder)
+
+    shutil.move(src_file, dst_file)
+
+import os
+
+def remove_file(file_path):
+    if os.path.isfile(file_path):
+        try:
+            os.remove(file_path)
+            print(f"Removed: {file_path}")
+        except Exception as e:
+            print(f"[ERROR] Failed to remove {file_path}: {e}")
+    else:
+        print(f"[INFO] File not found: {file_path}")
---
+++ SampleCode/StdDriver/PWM_AccumulatorINT_TriggerPDMA/main.c
@@ -0,0 +1,232 @@
+/**************************************************************************//**
+ * @file    main.c
+ * @version V1.00
+ * @brief   Demonstrate PWM accumulator interrupt trigger PDMA.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * @copyright (C) 2023 Nuvoton Technology Corp. All rights reserved.
+ *****************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include "NuMicro.h"
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* Global variables                                                                                        */
+/*---------------------------------------------------------------------------------------------------------*/
+#if (NVT_DCACHE_ON == 1)
+/* Data size (g_au16Period) < one cache line size (32B) => Non-cacheable should be ok. */
+NVT_NONCACHEABLE_INIT __attribute__((aligned(4))) static uint16_t g_au16Period[2] = {31999, 15999};
+#else
+__attribute__((aligned(4))) static uint16_t g_au16Period[2] = {31999, 15999};
+#endif
+
+static volatile uint32_t g_u32IsTestOver = 0;
+
+/**
+ * @brief       PDMA IRQ Handler
+ * @param       None
+ * @return      None
+ * @details     ISR to handle PDMA interrupt event
+ */
+void PDMA0_IRQHandler(void)
+{
+    uint32_t u32TimeOutCnt = SystemCoreClock; /* 1 second time-out */
+    uint32_t u32Status = PDMA_GET_INT_STATUS(PDMA0);
+
+    if (u32Status & PDMA_INTSTS_ABTIF_Msk)   /* abort */
+    {
+        if (PDMA_GET_ABORT_STS(PDMA0) & PDMA_ABTSTS_ABTIF0_Msk)
+            g_u32IsTestOver = 2;
+
+        PDMA_CLR_ABORT_FLAG(PDMA0, PDMA_ABTSTS_ABTIF0_Msk);
+    }
+    else if (u32Status & PDMA_INTSTS_TDIF_Msk)     /* done */
+    {
+        if (PDMA_GET_TD_STS(PDMA0) & PDMA_TDSTS_TDIF0_Msk)
+            g_u32IsTestOver = 1;
+
+        PDMA_CLR_TD_FLAG(PDMA0, PDMA_TDSTS_TDIF0_Msk);
+    }
+    else
+        printf("unknown interrupt %x !!\n", u32Status);
+
+    __DSB();
+    __ISB();
+
+    while (PDMA_GET_INT_STATUS(PDMA0))
+    {
+        if (--u32TimeOutCnt == 0)
+        {
+            printf("Wait for PDMA IntFlag time-out!\n");
+        }
+    }
+}
+
+static void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+    /*------------------------------------------------------------------------*/
+    /* Init System Clock                                                      */
+    /*------------------------------------------------------------------------*/
+    /* Enable HXT clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+
+    /* Wait for HXT clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
+
+    /* Set PCLK0 to HCLK/2 */
+    CLK_SET_PCLK0DIV(CLK_PCLKDIV_APB0DIV_DIV2);
+    /* Set PCLK1 to HCLK/2 */
+    CLK_SET_PCLK1DIV(CLK_PCLKDIV_APB1DIV_DIV2);
+
+    /* Set core clock */
+    CLK_SetCoreClock(FREQ_144MHZ);
+
+    /* Update System Core Clock */
+    SystemCoreClockUpdate();
+
+    /* Enable UART module clock */
+    SetDebugUartCLK();
+    /* Select PWM module clock source */
+    CLK_SetModuleClock(PWM1_MODULE, CLK_CLKSEL2_PWM1SEL_PCLK1, 0);
+
+    /* Enable PWM module clock */
+    CLK_EnableModuleClock(PWM1_MODULE);
+
+    /* Enable PDMA0 module clock */
+    CLK_EnableModuleClock(PDMA0_MODULE);
+
+    /* Enable GPIO clock */
+    CLK_EnableModuleClock(GPC_MODULE);
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    SetDebugUartMFP();
+    /* Set multi-function pin for PWM */
+    SET_PWM1_CH0_PC5();
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+int main(void)
+{
+    uint32_t u32NewCNR = 0;
+    uint32_t u32TimeOutCnt = 0;
+    /* Init System, IP clock and multi-function I/O */
+    SYS_Init();
+    /* Init Debug UART to 115200-8N1 for print message */
+    InitDebugUart();
+
+#if defined (__GNUC__) && !defined(__ARMCC_VERSION) && defined(OS_USE_SEMIHOSTING)
+    initialise_monitor_handles();
+#endif
+
+    printf("\n\nCPU @ %dHz(PLL@ %dHz)\n", SystemCoreClock, PllClock);
+    printf("+------------------------------------------------------------------------+\n");
+    printf("|              PWM AccumulatorINT TriggerPDMA Sample Code               |\n");
+    printf("+------------------------------------------------------------------------+\n");
+    printf("  This sample code demonstrate PWM1 channel 0 accumulator interrupt trigger PDMA.\n");
+    printf("  When accumulator interrupt happens, PWM1 channel 0 (PC5) period register will be updated \n");
+    printf("  to g_u32Count array content, 31999(0x7CFF), by PDMA.\n");
+
+    printf("\n\nPress any key to start PWM1 channel 0.(PC5)\n");
+    getchar();
+
+    /*--------------------------------------------------------------------------------------*/
+    /* Set the PWM1 Channel 0 as PWM output function.                                     */
+    /*--------------------------------------------------------------------------------------*/
+
+    /* Set PWM1 channel 0 output configuration */
+    PWM_ConfigOutputChannel(PWM1, 0, 300, 30);
+
+    /* Enable PWM Output path for PWM1 channel 0 */
+    PWM_EnableOutput(PWM1, PWM_CH_0_MASK);
+
+    /* Enable PWM1 channel 0 accumulator, interrupt count 50, accumulator source select to zero point */
+    PWM_EnableAcc(PWM1, 0, 50, PWM_IFA_ZERO_POINT);
+
+    /* Enable PWM1 channel 0 accumulator interrupt */
+    PWM_EnableAccInt(PWM1, 0);
+
+    /* Enable PWM1 channel 0 accumulator interrupt trigger PDMA */
+    PWM_EnableAccPDMA(PWM1, 0);
+
+    /* Enable Timer for PWM1 channel 0 */
+    PWM_Start(PWM1, PWM_CH_0_MASK);
+
+    /*--------------------------------------------------------------------------------------*/
+    /* Configure PDMA peripheral mode form memory to PWM                                   */
+    /*--------------------------------------------------------------------------------------*/
+    /* Open Channel 0 */
+    PDMA_Open(PDMA0, BIT0);
+
+    /* Transfer width is half word(16 bit) and transfer count is 1 */
+    PDMA_SetTransferCnt(PDMA0, 0, PDMA_WIDTH_16, 1);
+
+    /* Set source address as g_u32Count array(increment) and destination address as PWM1 channel 0 period register(no increment) */
+    PDMA_SetTransferAddr(PDMA0, 0, (uint32_t)&g_au16Period[0], PDMA_SAR_INC, (uint32_t) & (PWM1->PERIOD[0]), PDMA_DAR_FIX);
+
+    /* Select PDMA request source as PWM1_CH0_TX(PWM1 channel 0 accumulator interrupt) */
+    PDMA_SetTransferMode(PDMA0, 0, PDMA_PWM1_CH0_TX, FALSE, 0);
+
+    /* Set PDMA as single request type for PWM */
+    PDMA_SetBurstType(PDMA0, 0, PDMA_REQ_SINGLE, PDMA_BURST_1);
+
+    PDMA_EnableInt(PDMA0, 0, PDMA_INT_TRANS_DONE);
+    NVIC_EnableIRQ(PDMA0_IRQn);
+
+    g_u32IsTestOver = 0;
+
+    /* Wait for PDMA transfer done */
+    u32TimeOutCnt = SystemCoreClock; /* 1 second time-out */
+
+    while (g_u32IsTestOver != 1)
+    {
+        if (--u32TimeOutCnt == 0)
+        {
+            printf("Wait for PDMA transfer done time-out!\n");
+            goto lexit;
+        }
+    }
+
+    u32NewCNR = PWM_GET_CNR(PWM1, 0);
+    printf("\n\nPWM1 channel0 period register is updated to %d(0x%x)\n", u32NewCNR, u32NewCNR);
+    printf("Press any key to stop PWM1 channel 0.\n");
+    getchar();
+
+    /* Set PWM1 channel 0 loaded value as 0 */
+    PWM_Stop(PWM1, PWM_CH_0_MASK);
+
+    /* Wait until PWM1 channel 0 Timer Stop */
+    u32TimeOutCnt = SystemCoreClock; /* 1 second time-out */
+
+    while ((PWM1->CNT[0] & PWM_CNT_CNT_Msk) != 0)
+    {
+        if (--u32TimeOutCnt == 0)
+        {
+            printf("Wait for PWM stop time-out!\n");
+            break;
+        }
+    }
+
+lexit:
+
+    /* Disable Timer for PWM1 channel 0 */
+    PWM_ForceStop(PWM1, PWM_CH_0_MASK);
+
+    /* Disable PWM Output path for PWM1 channel 0 */
+    PWM_DisableOutput(PWM1, PWM_CH_0_MASK);
+
+    /* Disable PDMA NVIC */
+    NVIC_DisableIRQ(PDMA0_IRQn);
+
+    /* Close PDMA */
+    PDMA_Close(PDMA0);
+
+    /* Got no where to go, just loop forever */
+    while (1) ;
+}
+
+/*** (C) COPYRIGHT 2023 Nuvoton Technology Corp. ***/
