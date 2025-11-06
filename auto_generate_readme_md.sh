6cfe197ff8b87055530ec956ef9ff653d3fc78b8
Wayne <wclin@nuvoton.com>
2025-11-06 15:05:55 +0800

    update readme

--- auto_generate_readme_md.sh
+++ auto_generate_readme_md.sh
@@ -19,14 +19,14 @@ for dept in $(jq -r 'keys[]' "$JSON_FILE"); do
 
     # Markdown table header
     cat <<EOL >> "$README_FILE"
-| BSP     | NuEclipse | VSCode | IAR | MDK5 |
-|---------|-----------|--------|-----|------|
+| BSP     | NuEclipse | VSCode | IAR | MDK5 | Code Analysis |
+|---------|-----------|--------|-----|------|---------------|
 EOL
 
     # Loop through BSPs in this department
     bsp_list=$(jq -r --arg d "$dept" '.[$d][]' "$JSON_FILE")
     for bsp in $bsp_list; do
-        echo "| $bsp | ![NuEclipse](https://github.com/wosayttn/Gerrit_NuMicro/actions/workflows/NuEclipse.yml/badge.svg?branch=${dept}_$bsp) | ![VSCode](https://github.com/wosayttn/Gerrit_NuMicro/actions/workflows/VSCode.yml/badge.svg?branch=${dept}_$bsp) | ![IAR](https://github.com/wosayttn/Gerrit_NuMicro/actions/workflows/IAR.yml/badge.svg?branch=${dept}_$bsp) | ![MDK5](https://github.com/wosayttn/Gerrit_NuMicro/actions/workflows/MDK5.yml/badge.svg?branch=${dept}_$bsp) |" >> "$README_FILE"
+        echo "| $bsp | ![NuEclipse](https://github.com/wosayttn/Gerrit_NuMicro/actions/workflows/NuEclipse.yml/badge.svg?branch=${dept}_$bsp) | ![VSCode](https://github.com/wosayttn/Gerrit_NuMicro/actions/workflows/VSCode.yml/badge.svg?branch=${dept}_$bsp) | ![IAR](https://github.com/wosayttn/Gerrit_NuMicro/actions/workflows/IAR.yml/badge.svg?branch=${dept}_$bsp) | ![MDK5](https://github.com/wosayttn/Gerrit_NuMicro/actions/workflows/MDK5.yml/badge.svg?branch=${dept}_$bsp) | ![CodeAnalysis](https://github.com/wosayttn/Gerrit_NuMicro/actions/workflows/CodeAnalysis.yml/badge.svg?branch=${dept}_$bsp) |" >> "$README_FILE"
     done
 
     echo "" >> "$README_FILE"
