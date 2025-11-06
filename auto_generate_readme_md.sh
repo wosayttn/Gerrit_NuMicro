e923f74268b47ffee2b4987d3e1fd916ad0eb971
Wayne <wclin@nuvoton.com>
2025-11-06 22:20:38 +0800

    update style

--- auto_generate_readme_md.sh
+++ auto_generate_readme_md.sh
@@ -5,28 +5,33 @@ source ./config.sh   # must define JSON_FILE
 
 README_FILE="README.md"
 
+URL_PREFIX="https://github.com/wosayttn/Gerrit_NuMicro/actions/workflows"
+
 # Define workflow set
 WORKFLOWS=("NuEclipse" "VSCode" "IAR" "MDK5" "CodeAnalysis" "PrjChk")
 
 # Start README
 {
   echo "# NuMicro BSP Status"
-  echo
 } > "$README_FILE"
 
 # Table Header
 {
-  printf "| BSP "
-  for wf in "${WORKFLOWS[@]}"; do
-      printf "| %s " "$wf"
-  done
-  printf "|\n"
+  #printf "| BSP "
+  #for wf in "${WORKFLOWS[@]}"; do
+  #    printf "| %s " "$wf"
+  #done
+  #printf "|\n"
 
-  printf "|-----"
-  for _ in "${WORKFLOWS[@]}"; do
-      printf "|-----------"
-  done
-  printf "|\n"
+  #printf "|-----"
+  #for _ in "${WORKFLOWS[@]}"; do
+  #    printf "|-----------"
+  #done
+  #printf "|\n"
+  
+  printf "|Branch|Workflows|\n"
+  printf "|-----|------|\n"
+
 } >> "$README_FILE"
 
 # Process each department and BSP entries
@@ -36,12 +41,12 @@ for dept in $(jq -r 'keys[]' "$JSON_FILE"); do
 
       branch="${dept}_${bsp}"
 
-      printf "| %s " "$branch" >> "$README_FILE"
+      printf "| %s |" "$branch" >> "$README_FILE"
 
       for wf in "${WORKFLOWS[@]}"; do
-          printf "| [![](%s)](%s)" \
-            "https://github.com/wosayttn/Gerrit_NuMicro/actions/workflows/${wf}.yml/badge.svg?branch=$branch" \
-            "https://github.com/wosayttn/Gerrit_NuMicro/actions/workflows/${wf}.yml?query=branch:$branch"  >> "$README_FILE"
+          printf "[![](%s)](%s)" \
+            "$URL_PREFIX/${wf}.yml/badge.svg?branch=$branch" \
+            "$URL_PREFIX/${wf}.yml?query=branch:$branch"  >> "$README_FILE"
       done
 
       printf "|\n" >> "$README_FILE"
