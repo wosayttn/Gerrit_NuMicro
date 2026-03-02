4f5ff8b5fa40245fa35a505697bf6073f167e26f
Wayne Lin <wosayttn@gmail.com>
2026-03-02 20:10:21 +0800

    Commit at 2026-03-02T12:10:21Z

---
+++ SampleCode/NuMaker-M3334KI/MP3_Player/isr.c
@@ -0,0 +1,46 @@
+/*************************************************************************//**
+ * @file     isr.c
+ * @version  V3.00
+ * @brief    ISR source file.
+ *
+ * @copyright SPDX-License-Identifier: Apache-2.0
+ * @copyright Copyright (C) 2021 Nuvoton Technology Corp. All rights reserved.
+*****************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include "NuMicro.h"
+
+#include "config.h"
+
+extern uint32_t aPCMBuffer[2][PCM_BUFFER_SIZE];
+
+void PDMA0_IRQHandler(void)
+{
+    uint32_t u32Status = PDMA_GET_INT_STATUS(PDMA0);
+    if(u32Status & 0x2)    /* done */
+    {
+        /* Monitor SA register to check PDMA is working on which buffer */
+        if(PDMA0->DSCT[2].SA == (uint32_t)&aPCMBuffer[1][0])
+        {
+            /* Buffer 0 is empty */
+            PH4 = 1;
+        }
+        else
+        {
+            /* Buffer 1 is empty */
+            PH5 = 1;
+        }
+
+        PDMA_CLR_TD_FLAG(PDMA0, PDMA_TDSTS_TDIF2_Msk);
+    }
+    else if(u32Status & 0x400)     /* Timeout */
+    {
+        PDMA_CLR_TMOUT_FLAG(PDMA0, PDMA_TDSTS_TDIF2_Msk);
+        printf("PDMA Timeout!!\n");
+    }
+    else
+    {
+        printf("0x%x\n", u32Status);
+        while(1);
+    }
+}
