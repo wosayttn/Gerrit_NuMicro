396126fbb647306d128f044aaa4972b31db031f8
Wayne Lin <wosayttn@gmail.com>
2026-06-15 20:03:21 +0800

    Commit at 2026-06-15T12:03:21Z

---
+++ SampleCode/StdDriver/USBD_HID_Transfer_And_MSC/main.c
@@ -0,0 +1,236 @@
+/******************************************************************************
+* @file     main.c
+* @version  V1.00
+* $Revision: 11 $
+* $Date: 18/07/18 3:30p $
+* @brief
+*           Demonstrate how to implement a composite device. (HID Transfer and Mass storage)
+*           Transfer data between USB device and PC through USB HID interface.
+*           A windows tool is also included in this sample code to connect with a USB device.
+* @note
+* SPDX-License-Identifier: Apache-2.0
+* Copyright (C) 2018 Nuvoton Technology Corp. All rights reserved.
+*****************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+#include "HID_Transfer_and_MSC.h"
+#include "massstorage.h"
+
+#define CRYSTAL_LESS        1    /* CRYSTAL_LESS must be 1 if USB clock source is HIRC */
+#define TRIM_INIT           (SYS_BASE+0x118)
+
+#define DATA_FLASH_BASE  MASS_STORAGE_OFFSET
+int IsDebugFifoEmpty(void);
+extern uint8_t volatile g_u8Suspend;
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable HIRC clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+
+    /* Waiting for HIRC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    /* Switch HCLK clock source to HIRC and HCLK source divide 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+
+    /* Switch UART0 clock source to HIRC */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+
+    /* Enable UART0 clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Switch USB clock source to HIRC & USB Clock = HIRC / 1 */
+    CLK_SetModuleClock(USBD_MODULE, CLK_CLKSEL0_USBDSEL_HIRC, CLK_CLKDIV0_USB(1));
+
+    /* Enable USB clock */
+    CLK_EnableModuleClock(USBD_MODULE);
+
+    /* Update System Core Clock */
+    SystemCoreClockUpdate();
+
+    /* Set PB multi-function pins for UART0 RXD=PB.12 and TXD=PB.13 */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk))
+                    |(SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+void PowerDown()
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    printf("Enter power down ...\n");
+    while(!IsDebugFifoEmpty());
+
+    /* Wakeup Enable */
+    USBD_ENABLE_INT(USBD_INTEN_WKEN_Msk);
+
+    CLK_PowerDown();
+
+    /* Clear PWR_DOWN_EN if it is not clear by itself */
+    if(CLK->PWRCTL & CLK_PWRCTL_PDEN_Msk)
+        CLK->PWRCTL ^= CLK_PWRCTL_PDEN_Msk;
+
+    printf("device wakeup!\n");
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void)
+{
+#if CRYSTAL_LESS
+    uint32_t u32TrimInit;
+#endif
+
+    uint32_t au32Config[2];
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Init UART0 to 115200-8n1 for print message */
+    UART_Open(UART0, 115200);
+
+    printf("\n");
+    printf("+--------------------------------------------------------------+\n");
+    printf("|     NuMicro USB HID Transfer and MassStorage Sample Code     |\n");
+    printf("+--------------------------------------------------------------+\n");
+
+    /* Checking if flash page size matches with target chip's */
+    if( (GET_CHIP_SERIES_NUM == CHIP_SERIES_NUM_I) || (GET_CHIP_SERIES_NUM == CHIP_SERIES_NUM_G) )
+    {
+        if(FMC_FLASH_PAGE_SIZE != 2048)
+        {
+            /* FMC_FLASH_PAGE_SIZE is different from target device */
+            /* Please enable the compiler option PAGE_SIZE_2048 in fmc.h */
+            printf("FMC_FLASH_PAGE_SIZE is different from target device\n");
+            printf("Please enable the compiler option PAGE_SIZE_2048 in fmc.h\n");
+            while(SYS->PDID);
+        }
+    }
+    else
+    {
+        if(FMC_FLASH_PAGE_SIZE != 512)
+        {
+            /* FMC_FLASH_PAGE_SIZE is different from target device */
+            /* Please disable the compiler option PAGE_SIZE_2048 in fmc.h */    
+            printf("FMC_FLASH_PAGE_SIZE is different from target device\n");
+            printf("Please disable the compiler option PAGE_SIZE_2048 in fmc.h\n");
+            while(SYS->PDID);
+        }
+    }
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable FMC ISP function. Before using FMC function, it should unlock system register first. */
+    FMC_Open();
+
+    /* Check if Data Flash Size is 64K. If not, to re-define Data Flash size and to enable Data Flash function */
+    if (FMC_ReadConfig(au32Config, 2) < 0)
+        return -1;
+
+    /* Check if Data Flash is enabled (CONFIG0[0]) and is expected address (CONFIG1) */
+    if (((au32Config[0] & 0x01) == 1) || (au32Config[1] != DATA_FLASH_BASE) )
+    {
+        FMC_ENABLE_CFG_UPDATE();
+        au32Config[0] &= ~0x1;
+        au32Config[1] = DATA_FLASH_BASE;
+        if (FMC_WriteConfig(au32Config, 2) < 0)
+            return -1;
+
+        FMC_ReadConfig(au32Config, 2);
+
+        /* Check if Data Flash is enabled (CONFIG0[0]) and is expected address (CONFIG1) */
+        if (((au32Config[0] & 0x01) == 1) || (au32Config[1] != DATA_FLASH_BASE))
+        {
+            printf("Error: Program Config Failed!\n");
+            /* Disable FMC ISP function */
+            FMC_Close();
+            SYS_LockReg();
+            return -1;
+        }
+        /* To check if all the debug messages are finished */
+        while(!IsDebugFifoEmpty());
+
+        printf("chip reset\n");
+        /* Reset Chip to reload new CONFIG value */
+        SYS->IPRST0 = SYS_IPRST0_CHIPRST_Msk;
+    }
+    printf("NuMicro USB MassStorage Start!\n");
+
+    /* Open USB controller */
+    USBD_Open(&gsInfo, HID_MSC_ClassRequest, NULL);
+
+    USBD_SetConfigCallback(MSC_SetConfig);
+
+    /* Endpoint configuration */
+    HID_MSC_Init();
+
+    /* Start USB device */
+    USBD_Start();
+
+    NVIC_EnableIRQ(USBD_IRQn);
+
+#if CRYSTAL_LESS
+    /* Backup default trim */
+    u32TrimInit = M32(TRIM_INIT);
+#endif
+
+    /* Clear SOF */
+    USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+
+    while(1)
+    {
+#if CRYSTAL_LESS
+       /* Start USB trim if it is not enabled. */
+        if((SYS->HIRCTRIMCTL & SYS_HIRCTRIMCTL_FREQSEL_Msk) != 1)
+        {
+            /* Start USB trim only when SOF */
+            if(USBD->INTSTS & USBD_INTSTS_SOFIF_Msk)
+            {
+                /* Clear SOF */
+                USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+
+                /* Re-enable crystal-less */
+                SYS->HIRCTRIMCTL = 0x01;
+                SYS->HIRCTRIMCTL |= SYS_HIRCTRIMCTL_REFCKSEL_Msk;
+            }
+        }
+
+        /* Disable USB Trim when error */
+        if(SYS->HIRCTRIMSTS & (SYS_HIRCTRIMSTS_CLKERIF_Msk | SYS_HIRCTRIMSTS_TFAILIF_Msk))
+        {
+            /* Init TRIM */
+            M32(TRIM_INIT) = u32TrimInit;
+
+            /* Disable crystal-less */
+            SYS->HIRCTRIMCTL = 0;
+
+            /* Clear error flags */
+            SYS->HIRCTRIMSTS = SYS_HIRCTRIMSTS_CLKERIF_Msk | SYS_HIRCTRIMSTS_TFAILIF_Msk;
+
+            /* Clear SOF */
+            USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+        }
+#endif
+        /* Enter power down when USB suspend */
+        if(g_u8Suspend)
+            PowerDown();
+
+        MSC_ProcessCmd();
+    }
+}
+
+
+/*** (C) COPYRIGHT 2018 Nuvoton Technology Corp. ***/
+
---
+++ SampleCode/StdDriver/USBD_HID_Transfer_CTRL/main.c
@@ -0,0 +1,169 @@
+/******************************************************************************
+ * @file     main.c
+ * @version  V1.00
+ * $Revision: 12 $
+ * $Date: 18/07/26 11:39a $
+ * @brief    Demonstrate how to transfer data between USB device and PC through USB HID interface.
+ *           A windows tool is also included in this sample code to connect with a USB device.
+ *
+ * @note
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2018 Nuvoton Technology Corp. All rights reserved.
+ *****************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+#include "hid_transfer.h"
+
+#define CRYSTAL_LESS        1    /* CRYSTAL_LESS must be 1 if USB clock source is HIRC */
+#define TRIM_INIT           (SYS_BASE+0x118)
+
+extern uint8_t volatile g_u8Suspend;
+int IsDebugFifoEmpty(void);
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable HIRC clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+
+    /* Waiting for HIRC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    /* Switch HCLK clock source to HIRC and HCLK source divide 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+
+    /* Switch UART0 clock source to HIRC */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+
+    /* Enable UART0 clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Switch USB clock source to HIRC & USB Clock = HIRC / 1 */
+    CLK_SetModuleClock(USBD_MODULE, CLK_CLKSEL0_USBDSEL_HIRC, CLK_CLKDIV0_USB(1));
+
+    /* Enable USB clock */
+    CLK_EnableModuleClock(USBD_MODULE);
+
+    /* Update System Core Clock */
+    SystemCoreClockUpdate();
+
+    /* Set PB multi-function pins for UART0 RXD=PB.12 and TXD=PB.13 */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk))
+                    |(SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+
+void PowerDown()
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    printf("Enter power down ...\n");
+    while(!IsDebugFifoEmpty());
+
+    /* Wakeup Enable */
+    USBD_ENABLE_INT(USBD_INTEN_WKEN_Msk);
+
+    CLK_PowerDown();
+
+    /* Clear PWR_DOWN_EN if it is not clear by itself */
+    if(CLK->PWRCTL & CLK_PWRCTL_PDEN_Msk)
+        CLK->PWRCTL ^= CLK_PWRCTL_PDEN_Msk;
+
+    printf("device wakeup!\n");
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
+int32_t main(void)
+{
+#if CRYSTAL_LESS
+    uint32_t u32TrimInit;
+#endif
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+
+    /* Init UART0 to 115200-8n1 for print message */
+    UART_Open(UART0, 115200);
+
+    printf("\n");
+    printf("+--------------------------------------------------------+\n");
+    printf("|      NuMicro USB HID Transfer via Control Transfer     |\n");
+    printf("+--------------------------------------------------------+\n");
+    printf("Windows tool will Read and Write one pair of reports(periodic exchanges of reports).\n");
+
+    /* Open USB controller */
+    USBD_Open(&gsInfo, HID_ClassRequest, NULL);
+
+    /* Endpoint configuration */
+    HID_Init();
+
+    /* Start USB device */
+    USBD_Start();
+
+    NVIC_EnableIRQ(USBD_IRQn);
+
+#if CRYSTAL_LESS
+    /* Backup default trim */
+    u32TrimInit = M32(TRIM_INIT);
+#endif
+
+    /* Clear SOF */
+    USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+
+    while(1)
+    {
+#if CRYSTAL_LESS
+       /* Start USB trim if it is not enabled. */
+        if((SYS->HIRCTRIMCTL & SYS_HIRCTRIMCTL_FREQSEL_Msk) != 1)
+        {
+            /* Start USB trim only when SOF */
+            if(USBD->INTSTS & USBD_INTSTS_SOFIF_Msk)
+            {
+                /* Clear SOF */
+                USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+
+                /* Re-enable crystal-less */
+                SYS->HIRCTRIMCTL = 0x01;
+                SYS->HIRCTRIMCTL |= SYS_HIRCTRIMCTL_REFCKSEL_Msk;
+            }
+        }
+
+        /* Disable USB Trim when error */
+        if(SYS->HIRCTRIMSTS & (SYS_HIRCTRIMSTS_CLKERIF_Msk | SYS_HIRCTRIMSTS_TFAILIF_Msk))
+        {
+            /* Init TRIM */
+            M32(TRIM_INIT) = u32TrimInit;
+
+            /* Disable crystal-less */
+            SYS->HIRCTRIMCTL = 0;
+
+            /* Clear error flags */
+            SYS->HIRCTRIMSTS = SYS_HIRCTRIMSTS_CLKERIF_Msk | SYS_HIRCTRIMSTS_TFAILIF_Msk;
+
+            /* Clear SOF */
+            USBD->INTSTS = USBD_INTSTS_SOFIF_Msk;
+        }
+#endif
+        /* Enter power down when USB suspend */
+        if(g_u8Suspend)
+            PowerDown();
+    }
+}
+
+
+/*** (C) COPYRIGHT 2018 Nuvoton Technology Corp. ***/
+
